024efb0d6f7945367b3e477752baf54782cbc75a
Anthea <anthea4002@gmail.com>
2020-07-04 17:08:41 +0100

    Initial commit

---
+++ vue-cli/src/components/tables/VDataTable.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{ test }}</p>
+        <button @click="changeText">Change</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            test: "test"
+        }
+    },
+    methods: {
+        changeText() {
+            this.test = "test2"
+        }
+    }
+}
+</script>
