38d771b20a721b0317e26ab5b9c5f71288ff9214
Anthea <anthea4002@gmail.com>
2020-07-04 20:30:10 +0100

    Functionality minus ordering

--- vue-cli/src/components/tables/VDataTable.vue
+++ vue-cli/src/components/tables/VDataTable.vue
@@ -5,7 +5,7 @@
                 <td v-for="col in columns" :key="col.dataKey" :style="{textAlign: col.align}">{{ col.name }}</td>
             </tr>
             <tbody>
-                <template v-for="row in rows">
+                <template v-for="row in rows.slice(startVal, endVal)">
                     <tr class="tableRow" :key="row.code">
                         <td :style="{textAlign: computedStyle(0)}">{{ row.code }}</td>
                         <td :style="{textAlign: computedStyle(1)}">{{ formatRow(row.startDate, 1) }}</td>
@@ -14,14 +14,7 @@
                 </template>
             </tbody>
         </table>
-        <!-- <div v-for="col in columns" :key="col.dataKey">
-            {{ col.name }}
-        </div> -->
-
-            
-        <!-- <p>{{ name }}</p>
-        <p> {{rows[0].code}}</p>
-        <button @click="changeText">Change</button> -->
+        <button v-for="n in totalPages" :key="n" @click="page(n)">{{n}}</button>
     </div>
 </template>
 
@@ -30,7 +23,30 @@ export default {
     props: ['columns', 'rows', 'per-page','name'],
     data: function() {
         return {
-            test: "test"
+            startVal: 0
+        }
+    },
+    computed: {
+        totalPages: function() {
+            //Calculate number of pages based on total length of rows and entries to show
+            let totalRows = this.rows.length;
+            let perPage = this.perPage;
+            var total = Math.floor(totalRows/perPage);
+            if (totalRows%perPage != 0) {
+                total = total+1;
+            }
+            return total;
+        },
+        endVal: function() {
+            //calculate end of splice, check full length
+            let endVal = this.perPage
+            //Check there are enough rows first 
+            if (this.perPage < this.rows.length) {
+                endVal = this.startVal + this.perPage;
+            } else {
+                endVal = this.rows.length;
+            }
+            return endVal
         }
     },
     methods: {
@@ -39,6 +55,11 @@ export default {
         },
         computedStyle: function(colIndex) {
             return this.columns[colIndex].align
+        },
+        page: function(n) {
+            //Change page
+            this.startVal = (n-1)*(this.perPage)
+            console.log("next")
         }
     }
 }
